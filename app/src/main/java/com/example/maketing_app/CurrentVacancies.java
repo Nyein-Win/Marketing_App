4d183e35ab5e577e801ff195d8fc0779bb53a5d9
Zaw-Min-Htwe <zawminhtwe@sathapana.com.mm>
2022-11-11 15:34:57 +0630

    what are u doing

--- app/src/main/java/com/example/maketing_app/CurrentVacancies.java
+++ app/src/main/java/com/example/maketing_app/CurrentVacancies.java
@@ -1,19 +1,14 @@
 package com.example.maketing_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class Human_Resources extends AppCompatActivity {
-
+public class CurrentVacancies extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_human_resources);
-
+        setContentView(R.layout.activity_current_vacancies2);
     }
 }
