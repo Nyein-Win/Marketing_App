0d414c7ce51ef676d44cd2951566d3fe7c426009
nyein win <nyeinwin@sathapana.com.mm>
2022-12-12 16:19:13 +0630

    Marketing_App(Nyein WAn) 20221112 5:00 AM(All finished to start app)

--- app/src/main/java/com/example/maketing_app/About_Sathapana.java
+++ app/src/main/java/com/example/maketing_app/About_Sathapana.java
@@ -33,7 +33,6 @@ public class About_Sathapana extends AppCompatActivity {
             resources = context.getResources();
             setTitle(resources.getString(R.string.services));
             lang_selected=0;
-
             VPAdapter vpAdapter=new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
             vpAdapter.addFragment(new en_about_sathapana_fragment(),"About Sathapana");
             vpAdapter.addFragment(new award_fragment(),"Awards");
@@ -48,7 +47,6 @@ public class About_Sathapana extends AppCompatActivity {
             vpAdapter.addFragment(new about_sathapan_fragment(),"စထာပါနာ အကြောင်း");
             vpAdapter.addFragment(new my_award_fragment(),"ဆုများ");
             viewPager.setAdapter(vpAdapter);
-
         }
     }
 }
