f7a318cb3894b641995c15bfac402224bd0e2c34
Zaw-Min-Htwe <zawminhtwe@sathapana.com.mm>
2022-12-13 09:14:10 +0630

    I finish branches page

--- app/src/main/java/com/example/maketing_app/Second_Branches.java
+++ app/src/main/java/com/example/maketing_app/Second_Branches.java
@@ -3,7 +3,9 @@ package com.example.maketing_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,17 +13,53 @@ import android.widget.Button;
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class Second_Branches extends AppCompatActivity {
+    int lang_selected;
+
+    Context context;
+    Resources resources;
+
+    Button yangonbtn,mandalaybtn,bagobtn,ayeyarbtn,mawaybtn,sagainbtn,monbtn,shanbtn,kayinbtn,kayahbtn,
+            naypyitawbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_branches);
-
-        getSupportActionBar().setTitle("Branches");
+        setContentView(R.layout.activity_branches);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        yangonbtn=findViewById(R.id.yangobtn);
+        mandalaybtn=findViewById(R.id.mandalaybtn);
+        bagobtn=findViewById(R.id.bagobtn);
+        ayeyarbtn=findViewById(R.id.ayarbtn);
+        mawaybtn=findViewById(R.id.magwaybtn);
+        sagainbtn=findViewById(R.id.sagainbtn);
+        monbtn=findViewById(R.id.monbtn);
+        shanbtn=findViewById(R.id.shanbtn);
+        kayinbtn=findViewById(R.id.kayinbtn);
+        kayahbtn=findViewById(R.id.kayarbtn);
+        naypyitawbtn=findViewById(R.id.naypyitawbtn);
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button yangonbtn=findViewById(R.id.yangobtn);
+
+
+        if (LocaleHelper.getLanguage(Second_Branches.this).equalsIgnoreCase("en")) {
+            context = LocaleHelper.setLocale(Second_Branches.this, "en");
+            resources = context.getResources();
+            lang_selected = 0;
+            yangonbtn.setText(resources.getString(R.string.yan));
+//            mandalaybtn.setText(resources.getString(R.string.man));
+            setTitle(resources.getString(R.string.branches));
+            setString();
+
+
+        } else if (LocaleHelper.getLanguage(Second_Branches.this).equalsIgnoreCase("my")) {
+            context = LocaleHelper.setLocale(Second_Branches.this, "my");
+            resources = context.getResources();
+            lang_selected = 1;
+            yangonbtn.setText("မြန်မာ");
+            setTitle(resources.getString(R.string.branches));
+            setString();
+        }
+
 
         yangonbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +69,9 @@ public class Second_Branches extends AppCompatActivity {
                 Animatoo.animateSwipeRight(Second_Branches.this);
             }
         });
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button mandalaybtn=findViewById(R.id.mandalaybtn);
+
+
+
         mandalaybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +81,7 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button bagobtn=findViewById(R.id.bagobtn);
+
         bagobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,8 +90,8 @@ public class Second_Branches extends AppCompatActivity {
                 Animatoo.animateSwipeRight(Second_Branches.this);
             }
         });
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button ayarbtn=findViewById(R.id.ayarbtn);
-        ayarbtn.setOnClickListener(new View.OnClickListener() {
+
+        ayeyarbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Second_Branches.this,AyeyarwadiRegion.class);
@@ -60,8 +100,8 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button magwaybtn=findViewById(R.id.magwaybtn);
-        magwaybtn.setOnClickListener(new View.OnClickListener() {
+
+        mawaybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Second_Branches.this,MagwayRegion.class);
@@ -69,7 +109,7 @@ public class Second_Branches extends AppCompatActivity {
                 Animatoo.animateSwipeRight(Second_Branches.this);
             }
         });
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button sagainbtn=findViewById(R.id.sagainbtn);
+
         sagainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,7 +119,7 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button monbtn=findViewById(R.id.monbtn);
+
         monbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,7 +129,7 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button shanbtn=findViewById(R.id.shanbtn);
+
         shanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,7 +139,7 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button kayinbtn=findViewById(R.id.kayinbtn);
+
         kayinbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -109,8 +149,8 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button kayarbtn=findViewById(R.id.kayarbtn);
-        kayarbtn.setOnClickListener(new View.OnClickListener() {
+
+        kayahbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Second_Branches.this,KayarRegion.class);
@@ -119,8 +159,8 @@ public class Second_Branches extends AppCompatActivity {
             }
         });
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button naypyibtn=findViewById(R.id.naypyitawbtn);
-        naypyibtn.setOnClickListener(new View.OnClickListener() {
+
+        naypyitawbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Second_Branches.this,NaypyitawRegion.class);
@@ -128,5 +168,18 @@ public class Second_Branches extends AppCompatActivity {
                 Animatoo.animateSwipeRight(Second_Branches.this);
             }
         });
+    }
+    void setString() {
+        yangonbtn.setText(resources.getString(R.string.yan));
+        mandalaybtn.setText(resources.getString(R.string.man));
+        bagobtn.setText(resources.getString(R.string.bago));
+        ayeyarbtn.setText(resources.getString(R.string.aya));
+        mawaybtn.setText(resources.getString(R.string.mag));
+        sagainbtn.setText(resources.getString(R.string.saga));
+        monbtn.setText(resources.getString(R.string.mo));
+        shanbtn.setText(resources.getString(R.string.han));
+        kayinbtn.setText(resources.getString(R.string.kayin));
+        kayahbtn.setText(resources.getString(R.string.kayah));
+        naypyitawbtn.setText(resources.getString(R.string.nay));
     }
 }
